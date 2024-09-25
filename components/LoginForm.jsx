3c3084eae29ae30617717245e44af45d3eee4864
Md. Yamin <mohammadyamin80@gmail.com>
2024-09-25 20:46:52 +0600

    zod form validation added on login form

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -6,42 +6,49 @@ import { setCredentials } from "@/lib/features/auth/authSlice"
 import { useLoginMutation } from "@/lib/features/auth/authApiSlice"
 import { useRef, useState } from "react"
 import { useRouter } from "next/navigation"
-import { useAppDispatch } from "@/hooks/reduxHooks"; 
+import { useAppDispatch } from "@/hooks/reduxHooks";
 import { useToast } from "@/hooks/use-toast"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+
+const loginSchema = z.object({
+    email: z.string().email("Invalid email address").min(1, "Email is required"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+});
 
 
 export function LoginForm() {
-    const userRef = useRef()
-    const errRef = useRef()
     const { toast } = useToast()
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMsg, setErrorMsg] = useState('');
+
     const router = useRouter()
 
-    const [login, {isLoading}] = useLoginMutation()
+    const [login, { isLoading }] = useLoginMutation()
     const dispatch = useAppDispatch();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const onSubmit = async (data) => {
         try {
-            const userData = await login({email, password}).unwrap();
-            console.log(userData);
+            const userData = await login(data).unwrap();
+            // console.log(userData);
             dispatch(setCredentials({ user: userData.user_data, access_token: userData.access_token }));
             router.push("/dashboard");
         } catch (err) {
             // console.log(err.data.status_message);
-            setErrorMsg(err.data.status_message);
             toast({
                 variant: "destructive",
                 description: err.data.status_message,
             })
         }
     }
-
-
-
 
     return (
         <div className="flex flex-col gap-32 w-full lg:max-w-[500px]">
@@ -52,30 +59,30 @@ export function LoginForm() {
                 </p>
             </div>
             <div>
-                <div className="grid gap-4">
+                <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
                     <div className="grid gap-2">
                         <Label htmlFor="email" className="font-semibold">Email address</Label>
                         <Input
                             id="email"
                             type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
+                            {...register("email")}
                         />
+                        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
                     </div>
                     <div className="grid gap-2">
                         <div className="flex items-center">
                             <Label htmlFor="password" className="font-semibold">Password</Label>
 
                         </div>
-                        <Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        <Input id="password" type="password" {...register("password")} />
+                        {errors.password && <p className="text-red-500">{errors.password.message}</p>}
                     </div>
                     <div className="mt-20">
-                        <Button type="submit" onClick={handleSubmit} className="px-10 py-5 font-semibold">
-                            Sign in
+                        <Button type="submit" className="px-10 py-5 font-semibold" disabled={isLoading}>
+                        {isLoading ? "Signing in..." : "Sign in"}  
                         </Button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
