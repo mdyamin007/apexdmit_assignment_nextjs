2ace98cd196e40d21734d701ccd9ae3d04f5e54e
Md. Yamin <mohammadyamin80@gmail.com>
2024-09-25 21:21:15 +0600

    some bug fixed

--- app/StoreProvider.js
+++ app/StoreProvider.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore } from '../lib/store'
@@ -9,7 +9,13 @@ export default function StoreProvider({ children }) {
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore()
-    storeRef.current.dispatch(initAuth())
+
+    const user = localStorage.getItem("user");
+    const access_token = localStorage.getItem("access_token");
+
+    if (user && access_token) {
+      storeRef.current.dispatch(initAuth({ user: JSON.parse(user), access_token: access_token }));
+    }
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>
